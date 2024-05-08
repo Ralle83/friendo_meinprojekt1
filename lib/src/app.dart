8c0c877ea53454d3fa44fc4c7e61c63f1543a0b5
Ralle83 <ralf.magerl@gmx.de>
2024-05-08 14:41:33 +0200

    Splashscreen angepasst

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:friendo_meinprojekt/src/presentation/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
--- lib/src/presentation/splash_screen.dart
+++ lib/src/presentation/splash_screen.dart
@@ -4,9 +4,9 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF5F1), // Hintergrundfarbe des Splash Screens
+      backgroundColor: Color(0xFFFFF5F1), // Hintergrundfarbe des Splash Screens
       body: Center(
-        child: Image.asset('assets/logo.png'), // Dein App-Logo
+        child: Image.asset('assets/images/Logo.png'), // App-Logo
       ),
     );
   }
